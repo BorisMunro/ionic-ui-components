9d5797cb21f6d1430d80ce5840cc4dad83bf201f
BorisMunro <munroboris@gmail.com>
2024-02-02 03:08:45 +0800

    fix: added inline action sheet

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonActionSheet, IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { image } from 'ionicons/icons';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Action Sheet</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton id='open-action-sheet'>
+          <IonIcon icon={image} />
+          Open
+        </IonButton>
+        <IonActionSheet 
+          trigger='open-action-sheet'
+          header='Actions'
+          buttons={
+            [
+              {
+                text: 'Delete',
+                role: 'destructive',
+                data: {
+                  action: 'delete'
+                }
+              },
+              {
+                text: 'Cancel',
+                role: 'cancel',
+                data: {
+                  action: 'cancel',
+                },
+              },
+            ]
+          }
+        />  
       </IonContent>
     </IonPage>
   );
